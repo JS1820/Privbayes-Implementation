20bf5c8956686a893bb4eba9e5627ebe6448e533
Arunashok <108691415+0hex7@users.noreply.github.com>
2024-01-06 19:10:34 +0800

    Update __init__.py

--- Privbayes/code/util/__init__.py
+++ Privbayes/code/util/__init__.py
@@ -7,4 +7,4 @@ from util.factor_graph import FactorGraph
 from util.region_graph import RegionGraph
 from util.inference import FactoredInference
 from util.local_inference import LocalInference
-from util.torch_factor import Factor
+#from util.torch_factor import Factor
