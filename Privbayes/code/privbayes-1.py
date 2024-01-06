3dbee844ba75e90ce9d04cc46f9238749dc08be2
Arunashok <108691415+0hex7@users.noreply.github.com>
2024-01-06 18:25:49 +0800

    Update privbayes-1.py

--- Privbayes/code/privbayes-1.py
+++ Privbayes/code/privbayes-1.py
@@ -57,7 +57,7 @@ def worker(data, eps, seed):
 
 
 def get_dataset_file(dataset_name):
-    dataset_folder = '/Privbayes/data/'
+    dataset_folder = '/privbayes-implementation/Privbayes/data/'
     csv_file = f'{dataset_folder}{dataset_name}.csv'
     
     # Check if the CSV file exists
@@ -77,7 +77,7 @@ def preprocess(original_dataset):
     file_name = os.path.splitext(os.path.basename(original_dataset))[0]
 
     # Output directory path
-    output_directory = '/Privbayes/data/'
+    output_directory = '/privbayes-implementation/Privbayes/data/processed-output/'
 
     # Output file names
     processed_output_file = output_directory + f'processed_{file_name}.csv'
@@ -187,7 +187,7 @@ def privbayes_inference(domain, measurements, total, file_name):
                 synthetic.loc[idx,col] = np.random.choice(n, N, True, p)
     
     
-    output_folder = '/Privbayes/data/output/'  # Define the output folder path
+    output_folder = '/privbayes-implementation/Privbayes/data/synthetic-output/'  # Define the output folder path
     output_filename = f"synthetic_{file_name}.csv"  # Define the output filename
 
     # Combine output folder path and output filename
